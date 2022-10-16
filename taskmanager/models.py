1f69bdb71a043fea5e0209ef558aab3355ff7261
JorgenBrattang <jorgen.kvist@gmail.com>
2022-10-16 07:56:09 +0000

    Bug found, had to reset all installed libs

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,16 +1,17 @@
+
 from taskmanager import db
 
 
 class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
-    categery_name = db.Column(db.String(25), unique=True, nullable=False)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship(
         "Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return self
+        return self.category_name
 
 
 class Task(db.Model):
